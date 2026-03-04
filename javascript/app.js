f88f752116be7d7ca0307f4300668f3a06be5fb6
Lawliet-cpu <cateran.23@est.ucab.edu.ve>
2026-03-04 08:23:18 -0400

    ADD: empty list

--- javascript/app.js
+++ javascript/app.js
@@ -1,18 +1,8 @@
 import { Producto } from "./Producto.js";
 
 //Variables globales
-const listProduct = [];
+let listProduct = [];
 const cards = document.querySelectorAll('.card');
-
-//Obtener los datos del localStorage
-function getLocalStorage() {
-    return localStorage.getItem('Producto');
-}
-
-function jsonParse(value) {
-    return JSON.parse(value);
-}
-
 
 //Recorrer las cards
 cards.forEach((card, index) => {
@@ -20,8 +10,12 @@ cards.forEach((card, index) => {
     //Boton de cada card
     const btnSave = card.querySelector('.btn-add');
 
+    const contador = document.querySelector('.contador');
+    contador.innerHTML = `<p>contador: 0</p>`
+
     //Evento para añadir a Whislist
     btnSave.addEventListener('click', ()=> {
+
         //Declaracion de variables
         const nameProduct = card.querySelector('.name-product').textContent.trim();
         const description = card.querySelector('.description').textContent.trim();
@@ -33,34 +27,43 @@ cards.forEach((card, index) => {
         listProduct.push(producto);
 
         
-
         if(btnSave.textContent === "❤️ En tu Wishlist") {
-            btnSave.textContent = "Añadir a Whislist";
-            
-        } else {
 
-            //Agregar al localStorage
-            localStorage.setItem('Producto', JSON.stringify(listProduct));
+            //nueva lista sin el producto que se va a eliminar
+            const lista = listProduct.filter(nodo => nameProduct !== nodo.getNombre);
+            listProduct = lista;
 
-            //Hoja de estilos para que funcione
-            btnSave.textContent = "❤️ En tu Wishlist";
-            btnSave.style.marginTop = '10px';
-            btnSave.style.backgroundColor = 'var(--dark-green)';
-            btnSave.style.padding = '10px 50px 10px 50px';
-            btnSave.style.color = 'var(--razzmatazz)';
-            btnSave.style.border = 'none';
-            btnSave.style.fontWeight = 'bold';
-            btnSave.style.fontSize = '13.3333px';
-            btnSave.style.fontFamily = 'Arial';
-            btnSave.style.borderRadius = '3px';    
+            contador.innerHTML = `<p>contador: ${listProduct.length}</p>`;
+            btnSave.textContent = "Añadir a Whislist";  
+        }else { 
+                //Actualizar el contador
+                contador.innerHTML = `<p>contador: ${listProduct.length}</p>`;
+
+                //Hoja de estilos para que funcione
+                btnSave.textContent = "❤️ En tu Wishlist";
+                btnSave.style.marginTop = '10px';
+                btnSave.style.backgroundColor = 'var(--dark-green)';
+                btnSave.style.padding = '10px 50px 10px 50px';
+                btnSave.style.color = 'var(--razzmatazz)';
+                btnSave.style.border = 'none';
+                btnSave.style.fontWeight = 'bold';
+                btnSave.style.fontSize = '13.3333px';
+                btnSave.style.fontFamily = 'Arial';
+                btnSave.style.borderRadius = '3px';         
         }
 
-        const mano = getLocalStorage();
-        console.log('probando: ', mano);
+        //Agregar al localStorage
+        localStorage.setItem('Producto', JSON.stringify(listProduct));
     });
 })
 
-window.addEventListener('load', ()=> {
-    const mano = getLocalStorage();
-    console.log('probando: ', mano);
+const vaciar = document.querySelector('.delete');
+
+vaciar.addEventListener('click', (e)=> {
+    e.preventDefault();
+    listProduct = [];
+    const contador = document.querySelector('.contador');
+    contador.innerHTML = `<p>contador: ${listProduct.length}</p>`
+
+    localStorage.setItem('Producto', JSON.stringify(listProduct));
 });
